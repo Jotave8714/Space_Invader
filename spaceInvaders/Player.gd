206e5f68769466713395acc9a63d61190ef5a805
Jotave8714 <168234524+Jotave8714@users.noreply.github.com>
2024-08-19 07:57:35 -0300

    Add files via upload

---
+++ spaceInvaders/Player.gd
@@ -0,0 +1,18 @@
+extends Area2D
+
+@export var speed = 200
+var direction = Vector2.ZERO
+
+@onready var collision_rect: CollisionShape2D = %CollisionShape2D
+
+func _process(delta):
+	var input = Input.get_axis("move_left", "move_right")
+	if input > 0:
+		direction = Vector2.RIGHT
+	elif input < 0:
+		direction = Vector2.LEFT
+	else:
+		direction = Vector2.ZERO
+	
+	var delta_movement = direction.x * speed * delta
+	position.x += delta_movement
